5b6a6f2c68412c52bdb2da5037b2b243546d0933
Jan Fooken <git@bode.fun>
2023-04-28 11:37:01 +0200

    feat: 🥰 Implement missing identifiers

--- identifier.go
+++ identifier.go
@@ -16,10 +16,57 @@ type FileExtensionInDirectoryIdentifier struct {
 }
 
 func (i *FileExtensionInDirectoryIdentifier) Matches(potentialPath string) bool {
-	dir := filepath.Dir(potentialPath)
-	ext := filepath.Ext(potentialPath)
+	stat, err := os.Stat(potentialPath)
+	if err != nil {
+		return false
+	}
 
-	return i.Directory == dir && i.Extension == ext
+	if !stat.IsDir() {
+		return false
+	}
+
+	direntList, err := os.ReadDir(potentialPath)
+	if err != nil {
+		return false
+	}
+
+	for _, dirent := range direntList {
+		if filepath.Ext(dirent.Name()) == i.Extension {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (i *FileExtensionInDirectoryIdentifier) MatchesOptimistically(potentialRoot string) bool {
+	potentialPath := filepath.Join(potentialRoot, i.Directory)
+	potentialPath = filepath.Clean(potentialPath)
+	return i.Matches(potentialPath)
+}
+
+type FileNameInDirectoryIdentifier struct {
+	Directory string
+	Name      string
+}
+
+func (i *FileNameInDirectoryIdentifier) Matches(potentialPath string) bool {
+	dirName := filepath.Dir(potentialPath)
+	fileName := filepath.Base(potentialPath)
+
+	stat, err := os.Stat(potentialPath)
+	if err != nil {
+		return false
+	}
+
+	return !stat.IsDir() && dirName == i.Directory && fileName == i.Name
+}
+
+func (i *FileNameInDirectoryIdentifier) MatchesOptimistically(potentialRoot string) bool {
+	potentialPath := filepath.Join(potentialRoot, i.Directory, i.Name)
+	potentialPath = filepath.Clean(potentialPath)
+
+	return i.Matches(potentialPath)
 }
 
 type FileNameIdentifier struct {
@@ -28,15 +75,15 @@ type FileNameIdentifier struct {
 
 func (i *FileNameIdentifier) Matches(potentialPath string) bool {
 	fileName := filepath.Base(potentialPath)
+	stat, err := os.Stat(potentialPath)
+	if err != nil {
+		return false
+	}
 
-	return i.Name == fileName
+	return !stat.IsDir() && i.Name == fileName
 }
 
 func (i *FileNameIdentifier) MatchesOptimistically(potentialRoot string) bool {
 	potentialPath := filepath.Join(potentialRoot, i.Name)
-	if _, err := os.Stat(potentialPath); err != nil {
-		return false
-	}
-
-	return true
+	return i.Matches(potentialPath)
 }
